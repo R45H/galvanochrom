e5139988b78474ff43201945003783b29c76d4fe
unluckylucker <semka.shumilov@gmail.com>
2018-09-14 16:56:29 +0300

    slider

--- app/src/script.js
+++ app/src/script.js
@@ -2,6 +2,9 @@ $(function() {
 	//=require 'global/global.js'
 
 	/* Blocks will be inserted here */
+	(function() {
+		//=require 'blocks/slider/slider.js'
+	}());
 });
 
 //=require 'global/functions.js'
