bd4ec3d8067baeda4f18efe4dcbe2cfedc2c7765
unluckylucker <semka.shumilov@gmail.com>
2018-09-18 15:43:27 +0300

    slider

--- app/src/blocks/slider/slider.js
+++ app/src/blocks/slider/slider.js
@@ -1,4 +1,11 @@
-$('.slider').slick({
-		dots: true,
-});
+$('.slider').each(function () {
+	var
+		$this = $(this),
+		options = {};
 
+	if (!$this.hasClass('slider_dots_none')) {
+		options.dots = true;
+	}
+
+	$this.slick(options);
+});
