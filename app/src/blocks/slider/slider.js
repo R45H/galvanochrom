149531552caab738702f3b50c347c5608f764819
R45H <gmr45h@gmail.com>
2018-09-25 18:04:25 +0300

    Rework slider

--- app/src/blocks/slider/slider.js
+++ app/src/blocks/slider/slider.js
@@ -1,13 +1,27 @@
 $('.slider').each(function () {
 	var
 		$this = $(this),
-		options = {};
+		options = {
+			prevArrow: '<div class="slider__arrow slider__arrow_prev"></div>',
+			nextArrow: '<div class="slider__arrow slider__arrow_next"></div>'
+		};
 
 	if (!$this.hasClass('slider_dots_none')) {
 		options.dots = true;
+		options.dotsClass = 'slider__dots';
+		options.customPaging = function() {
+			return '<div class="slider__dot"></div>';
+		}
+	}
+
+	if ($this.hasClass('slider_height_full')) {
+
+		$this.on('init', function() {
+			$this
+				.find('.slider__dots')
+				.wrap("<div class='container slider__dots-wrap'></div>");
+		});
 	}
 
 	$this.slick(options);
 });
-
-$(".slick-dots").wrap("<div class='container'><div class='row'><div class='col-12'></div></div></div>");
