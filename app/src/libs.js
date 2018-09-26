63705783d6503304c757d50d8adb5e4472cdc270
R45H <gmr45h@gmail.com>
2018-09-26 15:35:57 +0300

    Add mask for tel input

--- app/src/libs.js
+++ app/src/libs.js
@@ -1,5 +1,6 @@
 //=require '../libs/jquery/dist/jquery.js'
 //=require '../libs/slick-carousel/slick/slick.js'
+//=require '../libs/jquery.maskedinput/dist/jquery.maskedinput.js'
 //=require '../libs/svg4everybody/dist/svg4everybody.js'
 
 //=require '../libs/jquery-ui/ui/version.js'
@@ -8,9 +9,7 @@
 //=require '../libs/jquery-ui/ui/safe-active-element.js'
 //=require '../libs/jquery-ui/ui/unique-id.js'
 //=require '../libs/jquery-ui/ui/widget.js'
-
 //=require '../libs/jquery-ui/ui/widgets/menu.js'
-
 //=require '../libs/jquery-ui/ui/escape-selector.js'
 //=require '../libs/jquery-ui/ui/form.js'
 //=require '../libs/jquery-ui/ui/form-reset-mixin.js'
