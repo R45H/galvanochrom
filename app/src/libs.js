11238ad306d86834dfa8245f36d2c7e0f0b6e5ad
unluckylucker <semka.shumilov@gmail.com>
2018-09-26 12:18:22 +0300

    fancybox

--- app/src/libs.js
+++ app/src/libs.js
@@ -1,6 +1,7 @@
 //=require '../libs/jquery/dist/jquery.js'
 //=require '../libs/slick-carousel/slick/slick.js'
 //=require '../libs/svg4everybody/dist/svg4everybody.js'
+//=require '../libs/fancybox/dist/jquery.fancybox.js'
 
 //=require '../libs/jquery-ui/ui/version.js'
 //=require '../libs/jquery-ui/ui/keycode.js'
